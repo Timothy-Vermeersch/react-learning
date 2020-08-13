30cc222d5607fc11350e9e94528742751219ba19
Timothy-Vermeersch <44079761+Timothy-Vermeersch@users.noreply.github.com>
2020-08-13 16:02:11 -0700

    Updates

---
+++ precache-manifest.cd9b4bd1162d8a9fa4590f8229591b00.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "00328e83e245d00a877cb0d94a72ec5b",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/index.html"
+  },
+  {
+    "revision": "59bd75f17c8bc5c7607f",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/css/main.72d1c995.chunk.css"
+  },
+  {
+    "revision": "eed28ffe09caff64414a",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/js/2.27efdaaa.chunk.js"
+  },
+  {
+    "revision": "dd207babaeb913476be46155cc504ef5",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/js/2.27efdaaa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "59bd75f17c8bc5c7607f",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/js/main.7b8237c7.chunk.js"
+  },
+  {
+    "revision": "7e5041457fe6fa7b4d2b",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/js/runtime-main.75649bed.js"
+  },
+  {
+    "revision": "46467e5b4e901eb37fc5b93cbad25ebc",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/media/background.46467e5b.jpg"
+  },
+  {
+    "revision": "dc3bc7e0167906baa5e15a790de67fac",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/media/example-one.dc3bc7e0.jpg"
+  },
+  {
+    "revision": "2c8f6ba71925e87cec78bb0d59fe3c15",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/media/example-three.2c8f6ba7.jpg"
+  },
+  {
+    "revision": "948bd2a464571b652398491b5e3c0d2c",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/media/example-two.948bd2a4.jpg"
+  },
+  {
+    "revision": "45f44ae67cdbb242c3aa480a4d4615d6",
+    "url": "/Timothy-Vermeersch.github.io/react-learning/static/media/githubPages.45f44ae6.png"
+  }
+]);
---
+++ service-worker.js
@@ -0,0 +1,39 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
+
+importScripts(
+  "/Timothy-Vermeersch.github.io/react-learning/precache-manifest.cd9b4bd1162d8a9fa4590f8229591b00.js"
+);
+
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/Timothy-Vermeersch.github.io/react-learning/index.html"), {
+  
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+});
